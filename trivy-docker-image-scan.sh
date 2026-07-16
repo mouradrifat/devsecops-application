5cb5efa90a8fba9be2d678e930a98820ad60b7ce
unknown <mourad.rifat@gmail.com>
2026-07-16 22:16:40 +0200

    fix trivity batsh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$dockerImageName=$(awk 'NR==1{print $2}' Dockerfile)
+dockerImageName=$(awk 'NR==1{print $2}' Dockerfile)
 echo $dockerImageName
 
 docker run --rm -v $WORKSPACE:/root/.cache aquasec/trivy image --severity CRITICAL --exit-code 1  $dockerImageName
